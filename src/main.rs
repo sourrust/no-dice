b38bd5fa19246887da0292891dfe9adcfaa9b01f
Jeremy Hull <sourdrums@gmail.com>
2016-07-10 20:39:54 -0700

    Add a very basic file that will be the CLI

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+extern crate no_dice;
+extern crate nom;
+
+use std::env;
+use nom::IResult;
+use no_dice::{DiceSize, Token};
+
+fn main() {
+  let mut args = Vec::new();
+
+  for arg in env::args().skip(1) {
+    let mut bytes = arg.into_bytes();
+
+    args.append(&mut bytes);
+  }
+
+  let result = no_dice::dice_roll(&args);
+
+  if let IResult::Done(_, tokens) = result {
+    for token in tokens {
+      println!("{:?}", token);
+
+      match token {
+        Token::DiceRoll(dice) => {
+          let roll = match dice.size {
+            DiceSize::Number(size) => no_dice::roll_multiple_die(
+                                        dice.count, size as i32),
+            DiceSize::Fate         => no_dice::roll_multiple_fate_die(
+                                        dice.count),
+          };
+
+          println!("{:?}", roll);
+        }
+        _                     => continue,
+      }
+    }
+  } else {
+    println!("{:?}", result);
+  }
+}
